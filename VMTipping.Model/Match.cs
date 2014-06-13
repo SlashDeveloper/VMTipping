653b4861117cf5ab438d0ca6efa02557c9e302d5
ragnarstolsmark <rstolsmark@live.no>
2014-06-13 14:50:53 +0200

    La til mvc-prosjekt

---
+++ VMTipping.Model/Match.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using VMTipping.Model;
+
+namespace VMTippekonkurranse.Models
+{
+    public class Match
+    {
+       
+        public int Id { get; set; }
+        public Team HomeTeam { get; set; }
+        public int HomeTeamId { get; set; }
+        public Team AwayTeam { get; set; }
+        public int HomeGoals { get; set; }
+        public int AwayGoals { get; set; }
+
+        public bool IsPlayed { get; set; }
+        [NotMapped]
+        public string Result
+        {
+            get
+            {
+                if (HomeGoals > AwayGoals) return "H";
+                if (HomeGoals == AwayGoals) return "U";
+                return "B";
+            }
+        }
+    }
+}
